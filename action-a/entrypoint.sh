6d0fdc854942ddf4574927f4f9ff293196a3468d
Alex Gonzalez <spokenbird@gmail.com>
2020-08-13 16:58:43 -0700

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+sh -c "echo Hello Code for America Engineers! My name is $INPUT_MY_NAME and you just witnessed a Github Action being run on a push!"
